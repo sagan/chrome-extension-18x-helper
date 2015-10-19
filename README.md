c4ddc599224630aff2b9886caae8c384f42e44d0
Jacques De SAGAN <jacquesde@sagan.me>
2015-10-19 11:20:41 +0800

    add some sites; make match process support sub domain

--- README.md
+++ README.md
@@ -1,11 +1,20 @@
 ﻿#chrome extension 「年齢確認ヘルパー」
 
-[Download from Chrome store](https://chrome.google.com/webstore/detail/%E5%B9%B4%E9%BD%A2%E7%A2%BA%E8%AA%8D%E3%83%98%E3%83%AB%E3%83%91%E3%83%BC/coedfpfbkbhbbakcbkmipnckgdndaeai)
+[Install from Chrome store](https://chrome.google.com/webstore/detail/%E5%B9%B4%E9%BD%A2%E7%A2%BA%E8%AA%8D%E3%83%98%E3%83%AB%E3%83%91%E3%83%BC/coedfpfbkbhbbakcbkmipnckgdndaeai)
 
 ## Supported Sites
+
+### General
 
 * Getchu
 * DMM
 * DLsite
 * DiGiket
+* Gyutto.com
 * amazon.co.jp
+* [とらのあな](http://www.toranoana.jp/)
+* [うらあきば.じぇいぴ～](http://www.ura-akiba.jp/)
+
+### ブランド OHP
+
+* [スミレ](http://s-mi-le.com/top)
--- src/background.js
+++ src/background.js
@@ -1,15 +1,18 @@
 
 var cookies = {
     // general sites
-	'www.dlsite.com': [{path: '/', name: 'adultchecked', value: '1'}],
-	'www.getchu.com': [{path: '/', name: 'getchu_adalt_flag', value: 'getchu.com'}],
-	'www.dmm.co.jp': [{path: '/', name: 'setover18', value: '1'}],
-	'www.dmm.com': [{domain: 'dmm.com', path: '/', name: 'ckcy', value: '1'}],
+    'www.dlsite.com': [{domain: 'dlsite.com', path: '/', name: 'adultchecked', value: '1'}],
+    'www.getchu.com': [{domain: 'getchu.com', path: '/', name: 'getchu_adalt_flag', value: 'getchu.com'}],
+    'dmm.co.jp': [{domain: 'dmm.co.jp', path: '/', name: 'setover18', value: '1'}],
     'dmm.com': [{domain: 'dmm.com', path: '/', name: 'ckcy', value: '1'}],
     'gyutto.com': [{domain: 'gyutto.com', path: '/', name: 'adult_check_flag', value: '1'}],
     'www.digiket.com': [{domain: 'digiket.com', path: '/', name: 'adult_check', value: '1'}],
+    'toranoana.jp': [{domain: 'toranoana.jp', path: '/', name: 'afg', value: '0'}], //とらのあな
+    'ura-akiba.jp': [{domain: 'ura-akiba.jp', path: '/', name: 'auth', value: '1'}], //うらあきば.じぇいぴ～
+    
     // brands sites
     's-mi-le.com': [{domain: 's-mi-le.com', path: '/', name: 'modal', value: 'off'}], // スミレ
+//  '': [{domain: '', path: '/', name: '', value: '1'}],
 };
 
 // see https://stackoverflow.com/questions/3689423/google-chrome-plugin-how-to-get-domain-from-url-tab-url
@@ -25,15 +28,32 @@ function getOrigin(uri) {
 
 chrome.webRequest.onBeforeRequest.addListener(function(details) {
     var domain = getDomain(details.url);
-    if( domain && cookies[domain] ) {
-        for(var i = 0; i < cookies[domain].length; i++) {
-           chrome.cookies.set({
-             url: details.url,
-             domain: cookies[domain][i].domain,
-             name: cookies[domain][i].name,
-             path: cookies[domain][i].path,
-             value: cookies[domain][i].value,
-           }, function() {}); 
+    var matchedDomain;
+    
+    if( domain ) {
+        if( cookies[domain] ) {
+            matchedDomain = domain;
+        } else {
+            var parts = domain.split('.');
+            if( parts.length > 2 ) {
+                parts.splice(0,1);
+                domain = parts.join('.');
+                if( cookies[domain]) {
+                    matchedDomain = domain;
+                }
+            }
+        }
+    }
+    
+    if( matchedDomain ) {
+        for(var i = 0; i < cookies[matchedDomain].length; i++) {
+            chrome.cookies.set({
+                url: details.url,
+                domain: cookies[matchedDomain][i].domain,
+                name: cookies[matchedDomain][i].name,
+                path: cookies[matchedDomain][i].path,
+                value: cookies[matchedDomain][i].value,
+            }, function() {}); 
         }
     }
 }, {urls: ["<all_urls>"], types: ["main_frame"]}, ["blocking"]);
